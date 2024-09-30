89cf67b3e969695a7900a89fe630f90094b2d135
SAINIKHILCHARY <madupusainikhil@gmail.com>
2024-09-30 18:27:31 +0530

    Delete Solution6.py

--- Solution6.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def min_coins(coins, sum):
-        dp = [float('inf')] * (sum + 1)
-        dp[0] = 0
-        for coin in coins:
-            for j in range(coin, sum + 1):
-                dp[j] = min(dp[j], dp[j - coin] + 1)
-
-        return dp[sum] if dp[sum] != float('inf') else -1
